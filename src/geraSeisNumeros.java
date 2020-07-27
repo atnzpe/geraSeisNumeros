b704f27820701a3eda4399757e293bea3bb7974d
Gleyson Atanazio <gleysonatanazio@gmail.com>
2020-07-27 19:11:09 -0300

    inclui interação e loop infinito

--- src/geraSeisNumeros.java
+++ src/geraSeisNumeros.java
@@ -1,16 +1,36 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class geraSeisNumeros {
 
     public static void main(String[] args) {
-        Random generate = new Random(); //Declarando variavel  generate do tipo Random;
-
-
-        for (int i = 0; i < 6; i++) {
-            int number = generate.nextInt(60);//declarei a variavel number e associei ao objeto generate;
-            System.out.println(number);
-
-        }
+        numeros();
     }
+    public static void numeros (){//declara o methodo da funcao numeros
+        Random generate = new Random(); //Declarando variavel  generate do tipo Random;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Qual o seu nome? ");
+        String name = scanner.nextLine();
+        System.out.println("Seja bem vindo " + name);
+
+        System.out.println("Quantos numeros aleatorios deseja escolher? ");
+        int x = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Entre quantos números? Por exemplo: 60 números.");
+        int y = Integer.parseInt(scanner.nextLine());
+
+
+
+        int i = 0;
+        while (i < x) {
+            int number = generate.nextInt(y);//declarei a variavel number e associei ao objeto generate;
+            System.out.println(number);
+            i++;
+        }
+        System.out.println("Você escolheu, " + x + " números " + " entre " + y + " números");
+
+        numeros();
+
+    }
 }
